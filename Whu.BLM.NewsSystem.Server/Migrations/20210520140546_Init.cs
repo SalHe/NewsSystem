271538b465dd8bd9ef28e21d7641bc1cebf1a207
SalHe <SalHe@qq.com>
2021-05-20 22:16:39 +0800

    Add-Migration

---
+++ Whu.BLM.NewsSystem.Server/Migrations/20210520140546_Init.cs
@@ -0,0 +1,207 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Whu.BLM.NewsSystem.Server.Migrations
+{
+    public partial class Init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "NewsCategories",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(type: "varchar(255)", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_NewsCategories", x => x.Id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Username = table.Column<string>(type: "varchar(255)", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Password = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    UserGroup = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "News",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Title = table.Column<string>(type: "varchar(255)", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    AbstractContent = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    OringinUrl = table.Column<string>(type: "varchar(255)", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    NewsCategoryId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_News", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_News_NewsCategories_NewsCategoryId",
+                        column: x => x.NewsCategoryId,
+                        principalTable: "NewsCategories",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "NewsUser",
+                columns: table => new
+                {
+                    VisitedById = table.Column<int>(type: "int", nullable: false),
+                    VisitedNewsId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_NewsUser", x => new { x.VisitedById, x.VisitedNewsId });
+                    table.ForeignKey(
+                        name: "FK_NewsUser_News_VisitedNewsId",
+                        column: x => x.VisitedNewsId,
+                        principalTable: "News",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_NewsUser_Users_VisitedById",
+                        column: x => x.VisitedById,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "NewsUser1",
+                columns: table => new
+                {
+                    LikedById = table.Column<int>(type: "int", nullable: false),
+                    LikedNewsId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_NewsUser1", x => new { x.LikedById, x.LikedNewsId });
+                    table.ForeignKey(
+                        name: "FK_NewsUser1_News_LikedNewsId",
+                        column: x => x.LikedNewsId,
+                        principalTable: "News",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_NewsUser1_Users_LikedById",
+                        column: x => x.LikedById,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "NewsUser2",
+                columns: table => new
+                {
+                    DislikedById = table.Column<int>(type: "int", nullable: false),
+                    DislikedNewsId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_NewsUser2", x => new { x.DislikedById, x.DislikedNewsId });
+                    table.ForeignKey(
+                        name: "FK_NewsUser2_News_DislikedNewsId",
+                        column: x => x.DislikedNewsId,
+                        principalTable: "News",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_NewsUser2_Users_DislikedById",
+                        column: x => x.DislikedById,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_News_NewsCategoryId",
+                table: "News",
+                column: "NewsCategoryId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_News_OringinUrl_Title",
+                table: "News",
+                columns: new[] { "OringinUrl", "Title" },
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_NewsCategories_Name",
+                table: "NewsCategories",
+                column: "Name",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_NewsUser_VisitedNewsId",
+                table: "NewsUser",
+                column: "VisitedNewsId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_NewsUser1_LikedNewsId",
+                table: "NewsUser1",
+                column: "LikedNewsId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_NewsUser2_DislikedNewsId",
+                table: "NewsUser2",
+                column: "DislikedNewsId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_Username",
+                table: "Users",
+                column: "Username",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "NewsUser");
+
+            migrationBuilder.DropTable(
+                name: "NewsUser1");
+
+            migrationBuilder.DropTable(
+                name: "NewsUser2");
+
+            migrationBuilder.DropTable(
+                name: "News");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+
+            migrationBuilder.DropTable(
+                name: "NewsCategories");
+        }
+    }
+}
