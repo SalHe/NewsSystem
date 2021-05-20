dbcbce80a2c86668d8958d225d19c96b26c91483
SalHe <SalHe@qq.com>
2021-05-20 19:07:34 +0800

    基本的实体设计

---
+++ Whu.BLM.NewsSystem.Shared/Entity/Content/News.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Whu.BLM.NewsSystem.Shared.Entity.Content
+{
+    /// <summary>
+    /// 新闻。
+    /// </summary>
+    public class News
+    {
+        public int Id { get; set; }
+
+        /// <summary>
+        /// 新闻标题。
+        /// </summary>
+        public string Title { get; set; }
+
+        /// <summary>
+        /// 摘要内容。
+        /// </summary>
+        public string AbstractContent { get; set; }
+
+        /// <summary>
+        /// 源URL。
+        /// </summary>
+        public string OringinUrl { get; set; }
+
+        /// <summary>
+        /// 类别。
+        /// </summary>
+        public NewsCategory Category { get; set; }
+    }
+}
---
+++ Whu.BLM.NewsSystem.Shared/Entity/Identity/User.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Whu.BLM.NewsSystem.Shared.Entity.Identity
+{
+    /// <summary>
+    /// 用户信息。
+    /// </summary>
+    public class User
+    {
+        public int Id { get; set; }
+
+        /// <summary>
+        /// 用户名
+        /// </summary>
+        public string Username { get; set; }
+
+        /// <summary>
+        /// 密码。
+        /// </summary>
+        public string Password { get; set; }
+
+        /// <summary>
+        /// 用户组别。
+        /// </summary>
+        public UserGroup UserGroup { get; set; }
+    }
+
+    public enum UserGroup
+    {
+        User, Admin, Publisher
+    }
+}
