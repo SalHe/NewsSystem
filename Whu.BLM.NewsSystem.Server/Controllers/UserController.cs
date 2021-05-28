88967358c2a0d9466539e3b6a674e06cea26986c
班坤杰 <2225080124@qq.com>
2021-05-28 11:23:34 +0800

    完成对新闻及其数据库的基本操作（增删改查）

---
+++ Whu.BLM.NewsSystem.Server/Controllers/UserController.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Whu.BLM.NewsSystem.Shared;
+using Whu.BLM.NewsSystem.Shared.Entity.Identity;
+
+
+namespace Whu.BLM.NewsSystem.Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UserController : ControllerBase
+    {
+        /*[HttpGet]
+        public User getUserInfo()
+        {
+            return new User();
+        }*/
+        
+    }
+}
