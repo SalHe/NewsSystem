dde82efab8896cf9b2dea090fa090d0046c44b8e
SalHe <SalHe@qq.com>
2021-06-04 11:16:30 +0800

    提取新闻类别相关功能到：INewsCategoryService

--- Whu.BLM.NewsSystem.Client/Program.cs
+++ Whu.BLM.NewsSystem.Client/Program.cs
@@ -7,6 +7,8 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Whu.BLM.NewsSystem.Client.Services;
+using Whu.BLM.NewsSystem.Client.Services.Impl;
 
 namespace Whu.BLM.NewsSystem.Client
 {
@@ -16,12 +18,13 @@ namespace Whu.BLM.NewsSystem.Client
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
-            
+
             builder.Services.AddScoped(sp =>
             {
                 string baseUr = builder.Configuration.GetSection("NewsServer")["Url"];
                 return new HttpClient {BaseAddress = new Uri(baseUr)};
             });
+            builder.Services.AddScoped<INewsCategoryService, NewsCategoryService>();
 
             // Add support for Ant Design
             builder.Services.AddAntDesign();
---
+++ Whu.BLM.NewsSystem.Client/Services/INewsCategoryService.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Whu.BLM.NewsSystem.Shared.Entity.Content;
+
+namespace Whu.BLM.NewsSystem.Client.Services
+{
+    public interface INewsCategoryService
+    {
+        Task<IList<NewsCategory>> GetNewsCategoriesAsync();
+    }
+}
