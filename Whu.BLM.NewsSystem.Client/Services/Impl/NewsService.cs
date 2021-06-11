846e34cb359c73b000d733c3966bb009cde0cffd
SalHe <SalHe@qq.com>
2021-06-11 08:08:02 +0800

    编辑新闻时初始获取新闻信息

--- Whu.BLM.NewsSystem.Client/Services/Impl/NewsService.cs
+++ Whu.BLM.NewsSystem.Client/Services/Impl/NewsService.cs
@@ -1,18 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Json;
 using System.Threading.Tasks;
+using Whu.BLM.NewsSystem.Client.Exceptions;
 using Whu.BLM.NewsSystem.Shared.Entity.Content;
 
 namespace Whu.BLM.NewsSystem.Client.Services.Impl
 {
     public class NewsService : INewsService
     {
-        private INewsCategoryService NewsCategoryService { get; }
+        private readonly HttpClient _httpClient;
 
-        public NewsService(INewsCategoryService newsCategoryService)
+        public NewsService(HttpClient httpClient)
         {
-            NewsCategoryService = newsCategoryService;
+            _httpClient = httpClient;
         }
 
         public IList<NewsCategory> CachedNewsCategories { get; set; }
@@ -34,10 +38,21 @@ namespace Whu.BLM.NewsSystem.Client.Services.Impl
             };
         }
 
-        public Task<News> GetNewsById(int id)
+        public async Task<News> GetNewsById(int id)
         {
-            // TODO 通过新闻ID获取新闻
-            return Task.FromResult(GenerateNews(id));
+            try
+            {
+                return await _httpClient.GetFromJsonAsync<News>($"api/news/{id}");
+            }
+            catch (HttpRequestException e)
+            {
+                if (e.StatusCode == HttpStatusCode.NotFound)
+                {
+                    throw new NewsNotFoundException("找不到对应的新闻");
+                }
+
+                throw;
+            }
         }
 
         public async Task<IList<News>> GetNewsListAsync(int page, int size)
